fce4c3d3f08d0e72cb463e7888f91ae1d512e27a
Steinway Wu <steinwaywhw@gmail.com>
2015-02-25 22:09:04 -0500

    clean up

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,16 +4,16 @@
 # Date: Feb 25 2015
 # 
 
-apt-get update 
-apt-get install -y wget libreadline-dev zlib1g-dev bison flex gcc make
+sudo apt-get update 
+sudo apt-get install -y wget libreadline-dev zlib1g-dev bison flex gcc make
 
 mkdir -p $SRCDIR
 wget -qO- https://ftp.postgresql.org/pub/source/v8.4.22/postgresql-8.4.22.tar.gz  | tar -xvz --strip-components=1 -C $SRCDIR
 
-groupadd -r postgres 
-useradd -r -g postgres postgres 
-mkdir -p /var/run/postgresql 
-chown -R postgres /var/run/postgresql 
-mkdir -p /var/lib/postgresql/data 
-chown -R postgres /var/lib/postgresql
+sudo groupadd -r postgres 
+sudo useradd -r -g postgres postgres 
+sudo mkdir -p /var/run/postgresql 
+sudo chown -R postgres /var/run/postgresql 
+sudo mkdir -p /var/lib/postgresql/data 
+sudo chown -R postgres /var/lib/postgresql
 
