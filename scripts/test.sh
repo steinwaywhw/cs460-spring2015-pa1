ae8ad8f87622461ec91cd8a24a8f911c59c546ec
Steinway Wu <steinwaywhw@gmail.com>
2015-02-26 11:35:05 -0500

    clean up

--- scripts/test.sh
+++ scripts/test.sh
@@ -21,7 +21,6 @@ fi
 
 source env.sh
 
-pg_ctl stop
 sudo rm -rf $PGDATA
 sudo su postgres -c ./init.sh 
 sudo su postgres -c ./query.sh
@@ -29,4 +28,3 @@ sudo su postgres -c ./query.sh
 cp /tmp/query_results.log $2/result.$1.$(date +%T).log
 cp /tmp/query_stats.log   $2/stats.$1.$(date  +%T).log
 
-pg_ctl stop
